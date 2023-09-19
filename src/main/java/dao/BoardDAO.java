aad643af6d571885ee70a7a8929db09631504e92
qwee0328 <qwee0328@naver.com>
2023-09-19 10:39:33 +0900

    freeboard Controller modified

--- src/main/java/dao/BoardDAO.java
+++ src/main/java/dao/BoardDAO.java
@@ -1,10 +1,17 @@
 package dao;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.naming.Context;
 import javax.naming.InitialContext;
 import javax.sql.DataSource;
+
+import dto.BoardDTO;
 
 public class BoardDAO {
 	private BoardDAO() {}
@@ -22,4 +29,42 @@ public class BoardDAO {
 	}
 	
 	// insert, selectBy~, selectAll, update, delete 로 함수명 통일 (최대한 sql 구문을 활용한 작명)
+	public List<BoardDTO> selectByCategory(String category, int start, int count) throws Exception{
+		String sql = "select row_number() over (order by cbSeq) rn, common_board.*  from common_board where cbCategory = ? order by cbSeq desc limit ?, ?;";
+		List<BoardDTO> list = new ArrayList<>();
+		try (Connection con = this.getConnection(); PreparedStatement pstat = con.prepareStatement(sql);) {
+			pstat.setString(1, category);
+			pstat.setInt(2, start);
+			pstat.setInt(3, count);
+			try (ResultSet rs = pstat.executeQuery()) {
+				while (rs.next()) {
+					int cbSeq = rs.getInt("cbSeq");
+					String cbID = rs.getString("cbID");
+					String cbNickname = rs.getString("cbNickname");
+					String cbTitle = rs.getString("cbTitle");
+					String cbContent = rs.getString("cbContent");
+					Timestamp cbWriteDate = rs.getTimestamp("cbWriteDate");
+					int cbView = rs.getInt("cbView");
+					String cbCategory = rs.getString("cbCategory");
+					int cbRecommend = rs.getInt("cbRecommend");
+					list.add(new BoardDTO(cbSeq, cbID, cbCategory, cbNickname, cbTitle, cbContent, cbWriteDate, cbView, cbRecommend));
+				}
+				return list;
+			}
+		}
+	}
+	
+	public int getRecordCount() throws Exception{
+		String sql = "select count(*) as count from common_board";
+		try (Connection con = this.getConnection();
+				PreparedStatement pstat = con.prepareStatement(sql);
+				ResultSet rs = pstat.executeQuery();) {
+
+			if (rs.next()) {
+				return rs.getInt("count");
+			} else {
+				return 0;
+			}
+		}
+	}
 }
