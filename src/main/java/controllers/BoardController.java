d1fb439b09f8360c6f8b0ba773934e9085d94c64
Jeen <eyungenius@gmail.com>
2023-09-20 16:37:33 +0900

    seoyoung reqeust revent 전으로

--- src/main/java/controllers/BoardController.java
+++ src/main/java/controllers/BoardController.java
@@ -103,46 +103,47 @@ public class BoardController extends HttpServlet {
 			} else if (cmd.equals("/delete.board")) {
 				// 게시글 삭제
 
+				
 			} else if (cmd.equals("/listing.board")) {
-				// 게시판 출력
-				String category = request.getParameter("category");
-				category = (category == null) ? "rhythm" : category;
+	            // 게시판 출력
+	            String category = request.getParameter("category");
+	            category = (category == null) ? "rhythm" : category;
 
-				String cpage = request.getParameter("cpage");
-				int currentPage = (cpage == null) ? 1 : Integer.parseInt(cpage);
-				request.getSession().setAttribute("lastPageNum", currentPage);
+	            String cpage = request.getParameter("cpage");
+	            int currentPage = (cpage == null) ? 1 : Integer.parseInt(cpage);
+	            request.getSession().setAttribute("lastPageNum", currentPage);
 
-				List<BoardDTO> list = new ArrayList<>();
+	            List<BoardDTO> list = new ArrayList<>();
 
-				// 검색한 카테고리, 키워드에 맞는 페이지 찾기
-				String search = request.getParameter("search");
-				String keyword = request.getParameter("keyword");
+	            // 검색한 카테고리, 키워드에 맞는 페이지 찾기
+	            String search = request.getParameter("search");
+	            String keyword = request.getParameter("keyword");
 
-				if (keyword == null || keyword.equals("")) {
-					// 검색 키워드가 넘어오지 않은 경우
-					list = dao.selectByCategory(category,
-							currentPage * Constants.RECORD_COUNT_PER_PAGE - Constants.RECORD_COUNT_PER_PAGE,
-							Constants.RECORD_COUNT_PER_PAGE);
-					request.setAttribute("recordTotalCount", dao.getRecordCount(category));
+	            if (keyword == null || keyword.equals("")) {
+	               // 검색 키워드가 넘어오지 않은 경우
+	               list = dao.selectByCategory(category,
+	                     currentPage * Constants.RECORD_COUNT_PER_PAGE - Constants.RECORD_COUNT_PER_PAGE,
+	                     Constants.RECORD_COUNT_PER_PAGE);
+	               request.setAttribute("recordTotalCount", dao.getRecordCount(category));
 
-				} else {
-					// 검색 키워드가 넘어온 경우
-				}
+	            } else {
+	               // 검색 키워드가 넘어온 경우
+	            }
 
-				List<BoardDTO> notiList = new ArrayList<>();
-				notiList = dao.selectByNoti();
+	            List<BoardDTO> notiList = new ArrayList<>();
+	            notiList = dao.selectByNoti();
 
-				request.setAttribute("cpage", cpage);
+	            request.setAttribute("cpage", cpage);
 
-				request.setAttribute("category", category);
-				request.setAttribute("type", "freeBoard");
-				request.setAttribute("notiList", notiList);
-				request.setAttribute("boardList", list);
-				request.setAttribute("recordCountPerPage", Constants.RECORD_COUNT_PER_PAGE);
-				request.setAttribute("naviCountPerPage", Constants.NAVI_COUNT_PER_PAGE);
+	            request.setAttribute("category", category);
+	            request.setAttribute("type", "freeBoard");
+	            request.setAttribute("notiList", notiList);
+	            request.setAttribute("boardList", list);
+	            request.setAttribute("recordCountPerPage", Constants.RECORD_COUNT_PER_PAGE);
+	            request.setAttribute("naviCountPerPage", Constants.NAVI_COUNT_PER_PAGE);
 
-				request.getRequestDispatcher("/board/boardList.jsp").forward(request, response);
-			} else if (cmd.equals("/write.board")) {
+	            request.getRequestDispatcher("/board/boardList.jsp").forward(request, response);
+	         } else if (cmd.equals("/write.board")) {
 				String menu = request.getParameter("menu");
 				System.out.println(menu);
 				String category = request.getParameter("category");
