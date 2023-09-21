c6ed59c99f1d59864ae74bd59e4fe308b9377b3b
권석경 <106041079+Rubiver@users.noreply.github.com>
2023-09-21 12:58:54 +0900

    trying to restore files(DAO,Controllers,Page)

---
+++ src/main/java/controllers/GameController.java
@@ -0,0 +1,100 @@
+package controllers;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.google.gson.Gson;
+
+import dao.GameDAO;
+import dto.GameRecordDTO;
+
+@WebServlet("*.game")
+public class GameController extends HttpServlet {
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		GameDAO dao = GameDAO.getInstance();
+		Gson gson = new Gson();
+		String cmd = request.getRequestURI();
+		System.out.println("game cmd: "+cmd);
+		
+		try {
+			if(cmd.equals("/moveToGamePage.game")) {
+				String gameName = request.getParameter("game");
+				String gameCategory = dao.selectByGameName(gameName);
+				String mID = "rubiver123";  //추후 mID 넘기는거로 변경해야함.
+				System.out.println(gameName);
+				System.out.println(gameCategory);
+				request.setAttribute("game",gameName);
+				request.setAttribute("category",gameCategory);
+				request.getRequestDispatcher("/game/GamePage_"+gameCategory+".jsp").forward(request, response);
+				
+			}else if(cmd.equals("/moveToCategory.game")) {
+				String category = request.getParameter("category");
+				request.setAttribute("category", category);
+				request.getRequestDispatcher("/game/GamePage_Category.jsp").forward(request, response);
+			}else if(cmd.equals("/favorite.game")) {
+				String mID = request.getParameter("mID"); //넘어오는 파라미터 이름은 추후 수정 가능.
+				String gameName = request.getParameter("gameName");
+				//ajax로 할듯?
+				int result = dao.insertFavorite(gameName, mID);
+				PrintWriter out = response.getWriter();
+				if(result>0) {
+					System.out.println("즐겨찾기 성공");
+					out.println("o");
+				}else {
+					System.out.println("즐겨찾기 실패");
+					out.println("x");
+				}
+			}else if(cmd.equals("/moveToBestGame.game")) {
+//				List<String> list = dao.selectFamousGame();
+//				request.setAttribute("best", list);
+				request.getRequestDispatcher("/game/GamePage_BestGame.jsp").forward(request, response);
+			}else if(cmd.equals("/checkFavorite.game")) {
+				String gameName = request.getParameter("game");
+				String mID = request.getParameter("mID");
+				int isFavorite = dao.selectFavorite(gameName, mID);
+				PrintWriter out = response.getWriter();
+				out.println(isFavorite);
+			}else if(cmd.equals("/deletefavorite.game")) {
+				String mID = request.getParameter("mID"); //넘어오는 파라미터 이름은 추후 수정 가능.
+				String gameName = request.getParameter("gameName");
+				int result = dao.deleteFavorite(gameName, mID);
+				PrintWriter out = response.getWriter();
+				if(result>0) {
+					System.out.println("즐겨찾기제거 성공");
+					out.println("o");
+				}else {
+					System.out.println("즐겨찾기제거 실패");
+					out.println("x");
+				}
+			}else if(cmd.equals("/getRecord.game")) {
+				List<GameRecordDTO> list = new ArrayList<>();
+				String gameName = request.getParameter("gameName");
+				list = dao.selectGameRecord(gameName);
+				System.out.println(list.get(0).getGameName());
+				PrintWriter out = response.getWriter();
+				out.println(gson.toJson(list));
+			}else if(cmd.equals("/getBestGame.game")) {
+				List<String> list = dao.selectFamousGame();
+				PrintWriter out = response.getWriter();
+				out.print(gson.toJson(list));
+			}
+		}catch(Exception e) {
+			e.printStackTrace();
+			response.sendRedirect("/error.html");
+		}
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
