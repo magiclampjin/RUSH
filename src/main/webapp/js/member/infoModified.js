9d953c5e6d8095dd04513fc21a0634e79ebbbfb6
qwee0328 <qwee0328@naver.com>
2023-10-05 09:40:56 +0900

    마이페이지 정규식 수정

--- src/main/webapp/js/member/infoModified.js
+++ src/main/webapp/js/member/infoModified.js
@@ -64,7 +64,7 @@ $(document).ready(function() {
 				pwCheck();
 
 			})
-			let regexPw = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[$@$!%*?&])[A-Za-z\d$@$!%*?&]{8,}$/; // 알파벳 대소문자, 숫자, 특수문자를 한 글자 이상 포함
+			let regexPw = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[$@$!%*?&#])[A-Za-z\d$@$!%*?&#]{8,}$/; // 알파벳 대소문자, 숫자, 특수문자를 한 글자 이상 포함
 			$("#newPw").on("keyup", function() {
 				let resultPw = regexPw.test($(this).val());
 				if (!resultPw) {
